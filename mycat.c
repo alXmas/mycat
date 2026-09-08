90f4d082ab687f60764371f5b0a023f83256022c
Alexandr <alexsandr.xmas@gmail.com>
2026-09-08 19:52:23 +0300

    update cmake file

--- mycat.c
+++ mycat.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
     int file_count = 1;
 
     while (file_count < argc) {
-      if (read_file(argv[file_count]) == EXIT_SUCCESS) {
+      if (read_file(argv[file_count]) != EXIT_SUCCESS) {
         result = EXIT_FAILURE;
       }
       file_count++;
