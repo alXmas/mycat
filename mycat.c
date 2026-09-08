bfc60765d4c9b1bc64a28c3e966e4471ec42008c
Alexandr <alexsandr.xmas@gmail.com>
2026-09-08 18:34:33 +0300

    Add read file and print to stdout

--- mycat.c
+++ mycat.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   int ch;
 
-  while ((ch = getchar()) != EOF) {
-    putchar(ch);
+  if (argc == 1) {
+    while ((ch = getchar()) != EOF) {
+      putchar(ch);
+    }
+  } else if (argc == 2) {
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    } else {
+      while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+      }
+    }
+    fclose(file);
+  } else {
+    printf("Too meny arcuments");
   }
-  return 0; 
+
+    return 0; 
 }
