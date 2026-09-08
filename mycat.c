d92be54299d8a1200b53d62d5ec60c643f31b7dd
Alexandr <alexsandr.xmas@gmail.com>
2026-09-08 18:10:03 +0300

    Add getchar

--- mycat.c
+++ mycat.c
@@ -1,1 +1,10 @@
-int main(int argc, char *argv[]) { return 0; }
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int ch;
+
+  while ((ch = getchar()) != EOF && ch != '\n') {
+    printf("char read  = %c, ASCII: %d\n", ch, ch);
+  }
+  return 0; 
+}
